03afee83bfadfe650170052b3f27ae48952b87a8
SeanQuinn <Seanquinn917@gmail.com>
2023-02-05 21:17:08 -0500

    commit tree 752215032b35af2c43dc7a97a2b131b81dba19ad

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ let length = word.length
+ for(i = 0; i<length/2; i ++) {
+    if(word[i] !== word[length -1 -i])
+         return false;
 }
+return true
+
+}
+
+
+// write a function that takes one string and returns true if string is a palindrome 
+// and false if not
+
+// input: 'dad'
+// output : true
+
+// input: 'snake'
+// output: false
+
+
+
+
+
+
+
+
+
 
 /* 
   Add your pseudocode here
